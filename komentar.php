7669361cc49fa57556b5a550ede7f63e374599e8
sindisrikandi <43509363+sindisrikandi@users.noreply.github.com>
2018-10-02 16:39:25 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,22 @@
+<form action="" method="POST">
+	<center>
+		<body bgcolor="#FFA9EF">
+		<br><br><br><br><br><br><br><br><br><br>
+	<table border="1" bgcolor="#d8f9ff">
+			<tr bgcolor="#fcf5b8">
+		<tr bgcolor="pink">
+	<td>komentar	:</td>
+	<td><input type="text" name="komentar"></td>
+		</tr>
+		<tr>
+			<td>
+			<input type="submit" name="submit" value="Hitung kata"></td></tr>
 <?php
+		if(isset($_POST['submit'])){
+	$komentar = $_POST['komentar'];
+	$hasil = str_word_count($komentar);
 
+	echo "<tr><td><center>jumlah kata : $hasil<center></td> </tr>";
+
+}
 ?>
